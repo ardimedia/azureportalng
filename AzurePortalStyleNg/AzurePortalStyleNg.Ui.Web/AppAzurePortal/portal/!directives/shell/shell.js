590b4678f5b7e3d59547783dcc5981f4e3f165d7
iwhp <harry@ardimedia.com>
2014-08-07 07:19:51 +0200

    Code Review.

---
+++ AzurePortalStyleNg/AzurePortalStyleNg.Ui.Web/AppAzurePortal/portal/!directives/shell/shell.js
@@ -0,0 +1,14 @@
+﻿/// <reference path="../../../../scripts/typings/angularjs/angular.d.ts" />
+(function () {
+    'use strict';
+
+    angular.module('appAzurePortal').directive('azurePortalShell', ['$window', azurePortalShell]);
+
+    function azurePortalShell($window) {
+        return {
+            scope: { vm: '=options' },
+            templateUrl: '/AppAzurePortal/portal/!directives/shell/shell.html'
+        };
+    }
+})();
+//# sourceMappingURL=shell.js.map
---
+++ AzurePortalStyleNg/AzurePortalStyleNg.Ui.Web/AppAzurePortalSample/controller/portalshell.ts
@@ -0,0 +1,36 @@
+﻿(function () {
+    'use strict';
+
+    angular.module('appAzurePortal').controller('portalshell', ['$scope', portalshell]);
+
+    function portalshell($scope) {
+        var vm = this;
+
+        vm.activate = activate;
+        vm.navigateTo = navigateTo;
+
+        vm.title = 'Azure Portal Sample';
+        vm.user = {
+            username: 'User Name',
+            emailaddress: 'email@domain.com'
+        };
+        vm.tiles = [
+            // size: normal (180x180), mini (180x90), small (90x90), herowide (540x360)
+            { title: 'Tile 1', subTitle: 'Some subtitle 1', size: 'normal', left: '0', top: '0', childId: 'tile1-nav' },
+            { title: 'Tile 2', subTitle: 'Some subtitle 2', size: 'normal', left: '180px', top: '0', childId: 'tile2-nav' }
+        ];
+        vm.views = [];
+
+        function activate() { }
+
+        function navigateTo(id) {
+            console.log(id);
+            if (id === 'tile1-nav') {
+                vm.views[0] = { title: 'Tile 1', subTitle: 'Some subtitle' };
+            }
+            if (id === 'tile2-nav') {
+                vm.views[0] = { title: 'Tile 2', subTitle: 'Some subtitle' };
+            }
+        }
+    }
+})();
