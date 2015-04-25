5b34e8ce2c300cd2d27bc46944fe4a7a5e179d35
Harry Pfleger <harry@ardimedia.com>
2015-04-25 08:16:03 +0200

    Add TypeScript files to NuGet packages (for intellisense to work). Deploy to NuGet.

--- AzurePortalStyleNg/AzurePortalStyleNg.Ui.Web/Properties/AssemblyInfo.cs
+++ AzurePortalStyleNg/AzurePortalStyleNg.Ui.Web/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AzurePortalStyleNg.Ui.Web")]
-[assembly: AssemblyDescription("Release Date: 2015-04-24")]
+[assembly: AssemblyDescription("Release Date: 2015-04-25")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Infoware, Schaan - Liechtenstein")]
 [assembly: AssemblyProduct("AzurePortalStyleNg.Ui.Web")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("0.1.0")]
-[assembly: AssemblyFileVersion("0.1.0")]
+[assembly: AssemblyVersion("0.1.2")]
+[assembly: AssemblyFileVersion("0.1.2")]
